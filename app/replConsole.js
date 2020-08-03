69aa03354c8765df9b5921994cb3cef53ac76190
lequanglam <29724654+lequanglam@users.noreply.github.com>
2020-08-03 23:48:59 +0700

    Experimental REPL usage in console

---
+++ app/replConsole.js
@@ -0,0 +1,30 @@
+let repl = require("repl");
+
+// Get ANSI color header based on config
+let redColorValue = parseInt(process.env.CONSOLE_LOG_COLOR.substr(0, 2));
+let greenColorValue = parseInt(process.env.CONSOLE_LOG_COLOR.substr(2, 2));
+let blueColorValue = parseInt(process.env.CONSOLE_LOG_COLOR.substr(4, 2));
+if (
+    isNaN(redColorValue) ||
+    isNaN(greenColorValue) ||
+    isNaN(blueColorValue)
+) {
+    redColorValue = 0;
+    greenColorValue = 255;
+    blueColorValue = 0;
+}
+let ANSI_COLOR_HEADER = `\x1B[38;2;${redColorValue};${greenColorValue};${blueColorValue}m`;
+
+global.replConsole = repl.start({
+    prompt: `${ANSI_COLOR_HEADER}localhost@c3c:js# `,
+    terminal: true,
+    useColors: true,
+    breakEvalOnSigint: true,
+    preview: true,
+    useGlobal: true,
+    completer: function completer(line) {
+        let cList = Object.keys(global);
+        let hits = cList.filter(c => c.startsWith(line));
+        return [hits.length ? hits : cList, hits.length === 1 ? hits[0] : line];
+    }
+});
--- index.js
+++ index.js
@@ -12,3 +12,19 @@ customEnv.env();
 global.ensureExists(path.join(__dirname, ".data"));
 
 let centralData = new (require("./app/storage/" + process.env.CENTRAL_STORAGE_TYPE))(__dirname);
+global.centralData = centralData;
+
+// Get a logger
+let Logging = require("./app/logging");
+let logger = new Logging();
+console.log = logger.log;
+
+// Output header
+logger.log("C3CBot v1.0-beta  Copyright (C) 2020  UIRI");
+logger.log("This program comes with ABSOLUTELY NO WARRANTY.");
+logger.log("This is free software, and you are welcome to redistribute it under certain conditions.");
+logger.log("This program is licensed using GNU GPL version 3, see the LICENSE file for details.");
+
+// Start REPL console
+logger.log("Starting REPL console...");
+require("./app/replConsole");
